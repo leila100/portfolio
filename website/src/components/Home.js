51cda7914bc29915e95f4422cc3e2e6856b0aa01
Leila <nb.leila10@gmail.com>
2019-05-04 10:09:26 -0500

    remove hr, add contact icons

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-import { Header, BG } from "../styles/headerStyles";
-import hr1 from "../assets/images/hr1.png";
+import { Header, BG, Icons } from "../styles/headerStyles";
 import backImage from "../assets/images/background.jpg";
 
 const Home = () => {
   return (
     <>
+      <Icons>
+        <i className='far fa-envelope' />
+        <i className='fab fa-linkedin' />
+        <i className='fab fa-github' />
+      </Icons>
       <Header>
         <h1>LEILA BERROUAYEL</h1>
         <h2>WEB/SOFTWARE DEVELOPER</h2>
         <p>Reliable - Committed - Dependable</p>
-        <img src={hr1} alt='horizontal Line' />
       </Header>
       <BG src={backImage} className='bk' alt='bg' />
     </>
--- website/src/styles/headerStyles.js
+++ website/src/styles/headerStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   margin-left: 20%;
-  margin-top: 150px;
+  margin-top: 200px;
   margin-bottom: 50px;
   text-align: center;
   color: white;
@@ -19,9 +19,9 @@ export const Header = styled.header`
   }
   img {
     text-align: center;
-    margin-top: 80px;
-    width: 80%;
-    height: 100px;
+    margin-bottom: 25px;
+    width: 60%;
+    height: 200px;
   }
 `;
 
@@ -34,4 +34,16 @@ export const BG = styled.img`
   left: 0;
   z-index: -1;
   opacity: 0.7;
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+
+  i {
+    margin: 20px 3%;
+    font-size: 3rem;
+    color: white;
+  }
 `;
